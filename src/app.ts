fcce79a49a5ba3b3dd0f15dd6f64686bf54e793d
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-18 15:56:04 -0300

    feat: create database postgres with prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+	data: {
+		name: 'Hugo Alves Varella',
+		email: 'hugovarellaa@gmail.com',
+	},
+})
