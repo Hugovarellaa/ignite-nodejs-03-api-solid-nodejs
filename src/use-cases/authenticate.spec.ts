98f48b24a8837ad184cffc42f9f5149b831d90ce
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-19 09:04:49 -0300

    feat: should not be able to authenticate with wrong password

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -46,4 +46,25 @@ describe('Authenticate User Case', () => {
 			expect(user).toHaveProperty('created_at')
 		}).rejects.toBeInstanceOf(InvalidCredentialsError)
 	})
+
+	it('should not be able to authenticate with wrong password', async () => {
+		expect(async () => {
+			await inMemoryUsersRepository.create({
+				name: 'John Doe',
+				email: 'john@example.com',
+				password_hash: await hash('123456', 6),
+			})
+
+			const { user } = await sut.execute({
+				email: 'john@example.com',
+				password: '321321',
+			})
+
+			expect(user).toHaveProperty('id')
+			expect(user).toHaveProperty('name')
+			expect(user).toHaveProperty('email')
+			expect(user).toHaveProperty('password_hash')
+			expect(user).toHaveProperty('created_at')
+		}).rejects.toBeInstanceOf(InvalidCredentialsError)
+	})
 })
