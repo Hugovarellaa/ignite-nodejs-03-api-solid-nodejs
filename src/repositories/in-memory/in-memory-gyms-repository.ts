aca004a867257d9a1ab7cff62e87516d045bf106
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-19 18:53:29 -0300

    feat: FetchNearByGymsUseCase

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { Gym, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
-import { IGymsRepository } from '../gyms-repository'
+import { IFindManyNearByParams, IGymsRepository } from '../gyms-repository'
 
 export class InMemoryGymRepository implements IGymsRepository {
 	public items: Gym[] = []
@@ -34,5 +35,19 @@ export class InMemoryGymRepository implements IGymsRepository {
 		return this.items
 			.filter((item) => item.title.includes(query))
 			.slice((page - 1) * 20, page * 20)
+	}
+
+	async findManyNearBy(params: IFindManyNearByParams): Promise<Gym[]> {
+		return this.items.filter((item) => {
+			const distance = getDistanceBetweenCoordinates(
+				{ latitude: params.latitude, longitude: params.longitude },
+				{
+					latitude: item.latitude.toNumber(),
+					longitude: item.longitude.toNumber(),
+				},
+			)
+
+			return distance < 10
+		})
 	}
 }
