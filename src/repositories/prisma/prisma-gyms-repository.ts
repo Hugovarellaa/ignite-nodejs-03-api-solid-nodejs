7bb67851ad7b3625734a254ee7c7518478e0ebaa
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-20 15:36:58 -0300

    feat: prisma-gyms-repository- method - findManyNearBy

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -23,13 +23,25 @@ export class PrismaGymsRepository implements IGymsRepository {
 	async searchMany(query: string, page: number): Promise<Gym[]> {
 		const gym = await prisma.gym.findMany({
 			where: {
-				title: query,
+				title: {
+					contains: query,
+				},
 			},
+			take: 20,
+			skip: (page - 1) * 20,
 		})
 		return gym
 	}
 
-	async findManyNearBy(params: IFindManyNearByParams): Promise<Gym[]> {
-		throw new Error('Method not implemented.')
+	async findManyNearBy({
+		latitude,
+		longitude,
+	}: IFindManyNearByParams): Promise<Gym[]> {
+		const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+		return gyms
 	}
 }
