fa200a1b71aec2acda957df0f0aab2600e17ef63
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-20 15:12:54 -0300

    fix: Update readme

---
+++ src/repositories/prisma/prisma-check-in-repository.ts
@@ -0,0 +1,37 @@
+import { prisma } from '@/lib/prisma'
+import { CheckIn, Prisma } from '@prisma/client'
+import { ICheckInRepository } from '../check-in-repository'
+
+export class PrismaCheckInsRepository implements ICheckInRepository {
+	async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+		throw new Error('Method not implemented.')
+	}
+
+	async findByUserIdOnDate(
+		userId: string,
+		date: Date,
+	): Promise<CheckIn | null> {
+		throw new Error('Method not implemented.')
+	}
+
+	async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+		throw new Error('Method not implemented.')
+	}
+
+	async countByUserId(userId: string): Promise<number> {
+		throw new Error('Method not implemented.')
+	}
+
+	async findById(id: string): Promise<CheckIn | null> {
+		const checkIn = await prisma.checkIn.findUnique({
+			where: {
+				id,
+			},
+		})
+		return checkIn
+	}
+
+	async save(checkIn: CheckIn): Promise<CheckIn> {
+		throw new Error('Method not implemented.')
+	}
+}
