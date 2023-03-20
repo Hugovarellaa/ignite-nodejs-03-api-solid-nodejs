586b86e89aa863034ae249c675f6236696623031
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-20 17:41:57 -0300

    feat: middleware verify-jwt

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,19 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-	await request.jwtVerify()
+	const getUserProfile = makeGetUserProfileUseCase()
 
-	console.log(request.user.sub)
+	const { user } = await getUserProfile.execute({
+		userId: request.user.sub,
+	})
 
-	return reply.status(201).send()
+	const userReturn = {
+		...user,
+		password_hash: undefined,
+	}
+
+	return reply.status(201).send({
+		user: userReturn,
+	})
 }
