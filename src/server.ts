bce7208e17b2012555b2539f1549940a741f36d9
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-07-15 16:43:12 -0300

    feat: First config Eslint + Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { app } from "./app";
+import { app } from './app'
 
-const PORT = 3333;
+const PORT = 3333
 
 app
-  .listen({
-    host: "0.0.0.0",
-    port: PORT,
-  })
-  .then(() => console.log(`🚀🚀 Server listening on port ${PORT} 🚀🚀`));
+	.listen({
+		host: '0.0.0.0',
+		port: PORT,
+	})
+	.then(() => console.log(`🚀🚀 Server listening on port ${PORT} 🚀🚀`))
